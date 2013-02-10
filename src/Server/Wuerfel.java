75d8854e3e22ad88484fe2c762dcdc9f36800d6e
Niboer <fragger.robin@web.de>
2013-02-10 21:17:13 +0100

    Jdoc Kommentare fuer die Klasse Wuerfel

--- src/Server/Wuerfel.java
+++ src/Server/Wuerfel.java
@@ -7,20 +7,30 @@ public class Wuerfel {
 
 	int w1; //Wuerfel 1
 	int w2; //Wuerfel 2
-	
+
+	/**
+	 * In dieser Methode werden zwei zufaellige Zahlenwerte generiert,
+	 * die zwischen 1 und 6 liegen und damit einem W6 Wuerfel entsprechen.
+	 * Diese Werte werden in den Attributen w1 und w2 gespeichert.
+	 */
 	public void wuerfeln(){
 		w1 =(int) ((Math.random()*120)%6)+1;
 		w2 =(int) ((Math.random()*120)%6)+1;	
 	}
 	
+	/**
+	 * Getter fuer w1
+	 * @return Wert fuer w1
+	 */
 	public int getWuerfel1(){
 		return w1;
 	}
 	
+	/**
+	 * Getter fuer w2
+	 * @return
+	 */
 	public int getWuerfel2(){
 		return w2;
-	}
-	
-	
-	
+	}	
 }
